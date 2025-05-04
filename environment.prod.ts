7628ff2e9af63c866e8869d037b2273a5f00e30e
myd-adc <malugamikola@gmail.com>
2025-05-04 13:49:12 +0300

    Нова збірка проекту з блокуванням перенаправлень

---
+++ environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.investo.app', // Змініть на реальний API URL, якщо він доступний
+  blockchainNetwork: 'testnet', // Встановлюємо testnet для початку
+  contractAddress: '0x1234567890123456789012345678901234567890', // Тестова адреса
+  infuraProjectId: 'your-infura-project-id',
+  walletConnectProjectId: 'your-walletconnect-project-id'
+};
