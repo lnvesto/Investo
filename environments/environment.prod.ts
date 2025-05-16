abceb48d2817ff31e2a6352a30e3043dd954f13f
myd-adc <94303506+myd-adc@users.noreply.github.com>
2025-05-16 11:28:07 +0000

    Deploying to gh-pages from @ lnvesto/Investo@1d2f7766f93de5b6cc51af62a4ace9f8a8f26a4a 🚀

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'investo-api.azurewebsites.net', 
+  apiUrl: 'investo-api.azurewebsites.net/api', 
   blockchainNetwork: 'testnet', 
   contractAddress: '0x1234567890123456789012345678901234567890', 
   infuraProjectId: 'your-infura-project-id',
