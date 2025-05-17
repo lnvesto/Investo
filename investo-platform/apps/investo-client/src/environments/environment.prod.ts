9e7252665789d5734f014b467d2c4a3446418278
myd-adc <malugamikola@gmail.com>
2025-05-17 22:52:06 +0300

    prod

--- investo-platform/apps/investo-client/src/environments/environment.prod.ts
+++ investo-platform/apps/investo-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.investo.app', 
+  apiUrl: 'https://investo-api.azurewebsites.net/api', 
   blockchainNetwork: 'testnet', 
   contractAddress: '0x1234567890123456789012345678901234567890', 
   infuraProjectId: 'your-infura-project-id',
